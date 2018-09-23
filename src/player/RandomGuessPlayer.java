3246b620156ecb69810d80347f8c2f9479e17a74
rmit-s3586931-kristin-stenland <s3586931@student.rmit.edu.au>
2018-09-23 17:54:04 +1000

    RandomGuessPlayer update

--- src/player/RandomGuessPlayer.java
+++ src/player/RandomGuessPlayer.java
@@ -1,6 +1,10 @@
 package player;
 
+import java.util.Iterator;
+import java.util.Random;
 import java.util.Scanner;
+
+import ship.Ship;
 import world.World;
 
 /**
@@ -9,28 +13,88 @@ import world.World;
  *
  * @author Youhan Xia, Jeffrey Chan
  */
-public class RandomGuessPlayer implements Player{
+public class RandomGuessPlayer implements Player {
+
+    static final int numShips = 5;
+    static final int maxLen = 5;
+    static final int[] rowDeltas = new int[]{1, 0, -1, 0, 1, 0, -1, 0};
+    static final int[] clnDeltas = new int[]{0, -1, 0, 1, 1, 0, -1, 0};
+    int rowSize = 0;
+    int clnSize = 0;
+    boolean isHex = false;
+    RandomGuessPlayer.OwnShip[] ownShip = new RandomGuessPlayer.OwnShip[5];
+    boolean[][] isguessed;
 
     @Override
     public void initialisePlayer(World world) {
-        // To be implemented.
+        this.rowSize = world.numRow;
+        this.clnSize = world.numColumn;
+        this.isHex = world.isHex;
+        this.isguessed = new boolean[this.rowSize][this.clnSize + (this.rowSize + 1) / 2];
+        int var2 = 0;
+
+        for (Iterator var3 = world.shipLocations.iterator(); var3.hasNext(); ++var2) {
+            World.ShipLocation var4 = (World.ShipLocation) var3.next();
+            this.ownShip[var2] = new RandomGuessPlayer.OwnShip();
+            this.ownShip[var2].ship = var4.ship;
+
+            for (int var5 = 0; var5 < this.ownShip[var2].ship.len(); ++var5) {
+                this.ownShip[var2].rowCdns[var5] = ((World.Coordinate) var4.coordinates.get(var5)).row;
+                this.ownShip[var2].clnCdns[var5] = ((World.Coordinate) var4.coordinates.get(var5)).column;
+                this.ownShip[var2].isdown[var5] = false;
+            }
+        }
     } // end of initialisePlayer()
 
     @Override
     public Answer getAnswer(Guess guess) {
-        // To be implemented.
+        Answer var2 = new Answer();
 
-        // dummy return
-        return null;
+        for(int var3 = 0; var3 < 5; ++var3) {
+            for(int var4 = 0; var4 < this.ownShip[var3].ship.len(); ++var4) {
+                if(guess.row == this.ownShip[var3].rowCdns[var4] && guess.column == this.ownShip[var3].clnCdns[var4]) {
+                    var2.isHit = true;
+                    this.ownShip[var3].isdown[var4] = true;
+                    boolean var5 = true;
+
+                    for(int var6 = 0; var6 < this.ownShip[var3].ship.len(); ++var6) {
+                        if(!this.ownShip[var3].isdown[var6]) {
+                            var5 = false;
+                        }
+                    }
+
+                    if(var5) {
+                        var2.shipSunk = this.ownShip[var3].ship;
+                    }
+
+                    return var2;
+                }
+            }
+        }
+
+        return var2;
     } // end of getAnswer()
 
 
     @Override
     public Guess makeGuess() {
-        // To be implemented.
+        Random var3 = new Random();
 
-        // dummy return
-        return null;
+        int var1;
+        int var2;
+        do {
+            var1 = var3.nextInt(this.rowSize);
+            var2 = var3.nextInt(this.clnSize);
+            if(this.isHex) {
+                var2 += (var1 + 1) / 2;
+            }
+        }
+        while(this.isguessed[var1][var2]);
+        Guess var4 = new Guess();
+        var4.row = var1;
+        var4.column = var2;
+        this.isguessed[var1][var2] = true;
+        return var4;
     } // end of makeGuess()
 
 
@@ -42,10 +106,29 @@ public class RandomGuessPlayer implements Player{
 
     @Override
     public boolean noRemainingShips() {
-        // To be implemented.
 
-        // dummy return
+        for(int var1 = 0; var1 < 5; ++var1) {
+            for(int var2 = 0; var2 < this.ownShip[var1].ship.len(); ++var2) {
+                if(!this.ownShip[var1].isdown[var2]) {
+                    return false;
+                }
+            }
+        }
         return true;
     } // end of noRemainingShips()
 
-} // end of class RandomGuessPlayer
+    private class OwnShip {
+        Ship ship;
+        int[] rowCdns;
+        int[] clnCdns;
+        boolean[] isdown;
+
+        private OwnShip() {
+            this.ship = null;
+            this.rowCdns = new int[]{-1, -1, -1, -1, -1};
+            this.clnCdns = new int[]{-1, -1, -1, -1, -1};
+            this.isdown = new boolean[]{true, true, true, true, true};
+        }
+
+    } // end of class RandomGuessPlayer
+}
